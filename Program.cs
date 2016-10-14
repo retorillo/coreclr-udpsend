ddebda9095bd28fbc9b02a78deab37f0015d0ac1
Retorillo <retorillo@outlook.com>
2016-10-15 02:58:07 +0900

    Fix exception handling

--- Program.cs
+++ Program.cs
@@ -32,16 +32,17 @@ public class Program
       canceller.Cancel();
     }
     catch (SocketException e) {
-      Console.WriteLine(e.Message);
-      Console.WriteLine();
-      return 1;
+      return HandleException(e);
     }
     catch (ArgumentException e) {
-      Console.WriteLine(e.Message);
-      Console.WriteLine();
-      return 1;
+      return HandleException(e);
     }
     return 0;
+  }
+  static int HandleException(Exception e) {
+    Console.WriteLine(e.Message);
+    Console.WriteLine();
+    return 1;
   }
   static IPAddress Resolve(string hostname, CancellationTokenSource canceller) {
     var task = Dns.GetHostEntryAsync(hostname);
@@ -52,24 +53,34 @@ public class Program
   static void RunAsClient(string remoteHost, int communicatePort, CancellationTokenSource canceller) {
     var udp = new UdpClient(communicatePort);
     var sendThread = new Thread(() => {
-      var remoteIP = Resolve(remoteHost, canceller);
-      var remoteEP = new IPEndPoint(remoteIP, communicatePort);
-      var c = 0;
-      while (!canceller.IsCancellationRequested) {
-        var bytes = encoding.GetBytes((c++).ToString());
-        var task = udp.SendAsync(bytes, bytes.Length, remoteEP);
-        task.Wait(canceller.Token);
-        Console.WriteLine("Data sent: " + c);
-        Thread.Sleep(1000);
+      try {
+        var remoteIP = Resolve(remoteHost, canceller);
+        var remoteEP = new IPEndPoint(remoteIP, communicatePort);
+        var c = 0;
+        while (!canceller.IsCancellationRequested) {
+          var bytes = encoding.GetBytes((c++).ToString());
+          var task = udp.SendAsync(bytes, bytes.Length, remoteEP);
+          task.Wait(canceller.Token);
+          Console.WriteLine("Data sent: " + c);
+          Thread.Sleep(1000);
+        }
       }
+      catch (OperationCanceledException) {
+        return;
+      } 
     });
     var echoBackThread = new Thread(() => {
-      while (!canceller.IsCancellationRequested) {
-        var task = udp.ReceiveAsync();
-        task.Wait(canceller.Token);
-        var text = encoding.GetString(task.Result.Buffer);
-        Console.WriteLine("Data recv :" + text);
-        Thread.Sleep(1);
+      try {
+        while (!canceller.IsCancellationRequested) {
+          var task = udp.ReceiveAsync();
+          task.Wait(canceller.Token);
+          var text = encoding.GetString(task.Result.Buffer);
+          Console.WriteLine("Data recv: " + text);
+          Thread.Sleep(1);
+        }
+      }
+      catch (OperationCanceledException) {
+        return;
       }
     });
     sendThread.Start();
@@ -79,25 +90,35 @@ public class Program
     var udp = new UdpClient(listenPort);
     var echoQueue = new Queue<UdpReceiveResult>();
     var listenThread = new Thread(() => {
-      Console.WriteLine("Listening...");
-      while (!canceller.IsCancellationRequested) {
-        var task = udp.ReceiveAsync();
-        task.Wait(canceller.Token);
-        var text = encoding.GetString(task.Result.Buffer);
-        Console.WriteLine("Data recv: " + text + " (" 
-          + task.Result.RemoteEndPoint.Address.ToString()
-          + ":" + task.Result.RemoteEndPoint.Port + ")");
-        echoQueue.Enqueue(task.Result);
-        Thread.Sleep(1);
+      try {
+        Console.WriteLine("Listening...");
+        while (!canceller.IsCancellationRequested) {
+          var task = udp.ReceiveAsync();
+          task.Wait(canceller.Token);
+          var text = encoding.GetString(task.Result.Buffer);
+          Console.WriteLine("Data recv: " + text + " (" 
+            + task.Result.RemoteEndPoint.Address.ToString()
+            + ":" + task.Result.RemoteEndPoint.Port + ")");
+          echoQueue.Enqueue(task.Result);
+          Thread.Sleep(1);
+        }
+      }
+      catch (OperationCanceledException) {
+        return;
       }
     });
     var echoThread = new Thread(() => {
-      while (!canceller.IsCancellationRequested) {
-        Thread.Sleep(1);
-        if (echoQueue.Count == 0) continue; 
-        var result = echoQueue.Dequeue();
-        var task = udp.SendAsync(result.Buffer, result.Buffer.Length, result.RemoteEndPoint);
-        task.Wait(canceller.Token);
+      try {
+        while (!canceller.IsCancellationRequested) {
+          Thread.Sleep(1);
+          if (echoQueue.Count == 0) continue; 
+          var result = echoQueue.Dequeue();
+          var task = udp.SendAsync(result.Buffer, result.Buffer.Length, result.RemoteEndPoint);
+          task.Wait(canceller.Token);
+        }
+      }
+      catch (OperationCanceledException) {
+        return;
       }
     });
     listenThread.Start();
